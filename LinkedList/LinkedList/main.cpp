555a1df462602a2717262b86025df3c09ccaffc1
JA-EMIN <jaemin.jang.jj@gmail.com>
2020-04-29 03:22:36 +0900

    LinkedList first

---
+++ LinkedList/LinkedList/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+int main()
+{
+	CLinkedList<int> listInt;
+	for (int i = 0; i < 100; i++)
+	{
+		listInt.push_back(i);
+	}
+	cout << "Size : " << listInt.size() << endl;
+	return 0;
+}
